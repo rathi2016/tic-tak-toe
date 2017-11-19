c24af45d18759a170507709a9508e86be2f3b0cc
Rathi <github email address>
2017-11-18 22:57:47 -0500

    required componet defined in app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      gameBoard: [
+        ' ',' ',' ',
+        ' ,',' ',' ',
+        ' ',' ',' '
+      ]
+    }
+  }
   render() {
     return (
-      <div>Hello !</div>
+      <div className="container">
+        <div className="menu">
+          <h1>Tic-Toe-toe</h1>
+          <Announcement/>
+          <ResetButton/>
+        </div>
+         {this.stat.gameBoard.map(function(value, i){
+           <Tile/>
+         })}
+      </div>
     );
   }
 }
