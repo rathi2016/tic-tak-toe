2d4347abd4cb552e6860cd21b0cbb975216b937b
Rathi <github email address>
2017-11-24 10:56:46 -0500

    adding board component

---
+++ src/components/board.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import Square from './Square';
+
+export default class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+  }
