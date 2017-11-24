34188b2707e108140318388de2244c4577101b69
Rathi <github email address>
2017-11-24 11:02:03 -0500

    working on the game

---
+++ src/components/Game.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Board from './board';
+import calculateWinner from '../util/CalculateWinner';
+
+export default class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(i) {
+      // const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        xIsNext: !this.state.xIsNext,
+        stepNumber: history.length,
+      });
+    }
