02e135a9fefcea65fb76792664a393523facd6ed
Rathi <github email address>
2017-11-24 10:58:45 -0500

    made board now going to add sqaure

--- src/components/board.js
+++ src/components/board.js
@@ -12,4 +12,28 @@ export default class Board extends React.Component {
   }
 
   render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
+}
+Board.propTypes = {
+  squares: PropTypes.array,
+  handleClick: PropTypes.func,
+};
